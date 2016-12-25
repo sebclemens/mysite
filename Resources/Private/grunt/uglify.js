9ae3b6cf6fa6b49197e8f4abcabc0d01acad149e
Sebastian Clemens <mail@sebastian-clemens.de>
2016-12-25 19:20:31 +0100

    Adding foundation's js files and jQuery

--- Resources/Private/grunt/uglify.js
+++ Resources/Private/grunt/uglify.js
@@ -1,6 +1,8 @@
 module.exports = {
     main: {
         src: [
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/foundation-sites/dist/js/foundation.js',
             'JavaScript/*.js'
         ],
         dest: '../Public/JavaScript/main.min.js'
