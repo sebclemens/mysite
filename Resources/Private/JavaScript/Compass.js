8e8a4a98ecad58799afa54969540b148a265bdb3
Sebastian Clemens <mail@sebastian-clemens.de>
2016-12-26 04:13:06 +0100

    General improvements

--- Resources/Private/JavaScript/Compass.js
+++ Resources/Private/JavaScript/Compass.js
@@ -3,25 +3,28 @@
     $.fn.compass = function() {
 
         // Load
-        this.animate({"opacity": 1, "margin-top": "9rem"}, 2000, function() {
+        this.animate({"opacity": 1, "margin-top": "13rem"}, 2000, function() {
             $(this).find("li").each(function(index) {
                 $(this).css("transform", "rotate("+ index * -45  +"deg) translateY(-8rem)")
                 .css("opacity", 1); 
             });
         });
 
+        var oldcolor;
         this.find("a").hover(function() {
+            oldcolor = $(this).css("background-color");
             $(this).css("background-color", "#000")
             .css("border-radius", "100%");
             var par = $(this).parent().parent().parent();
             if(!par.children('.main-overlay').length) {
-                par.append("<div class=\"main-overlay\">"+ $(this).attr("title")  +"</div>");
+                par.append("<div class=\"main-overlay\"></div>");
             }
+            par.find(".main-overlay").text($(this).attr("title")).fadeIn("fast");
         }, function() {
-            $(this).css("background-color", "blue")
+            $(this).css("background-color", oldcolor)
             .css("border-radius", "100% 100% 100% 0");
             var par = $(this).parent().parent().parent();
-            par.find(".main-overlay").remove();
+            par.find(".main-overlay").fadeOut("fast");
         });
     }
 
